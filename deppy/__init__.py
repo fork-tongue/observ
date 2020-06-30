19faf2c95e7b6c4d7a1b7dd77ec181d6aa012163
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2020-06-30 16:20:28 +0200

    watchers are complete

--- deppy/__init__.py
+++ deppy/__init__.py
@@ -40,32 +40,53 @@ def get_dep(obj, key=None) -> "Dep":
     return obj.__deps__[key]
 
 
+def traverse(obj):
+    _traverse(obj, set())
+
+
+def _traverse(obj, seen):
+    val_iter = None
+    if isinstance(obj, dict):
+        val_iter = iter(obj.values())
+    elif isinstance(obj, (list, tuple, set)):
+        val_iter = iter(obj)
+    if val_iter:
+        for v in val_iter:
+            _traverse(v, seen)
+
+
 class Watcher:
-    def __init__(self, fn, lazy=True) -> None:
+    def __init__(self, fn, lazy=True, deep=False, callback=None) -> None:
         self.id = next(_ids)
         self.fn = fn
         self._deps, self._new_deps = set(), set()
 
+        self.callback = callback
+        self.deep = deep
         self.lazy = lazy
         self.dirty = self.lazy
         self.value = None if self.lazy else self.get()
 
     def update(self) -> None:
-        if self.lazy:
-            self.dirty = True
-        else:
-            self.value = self.get()
+        self.dirty = True
+        if not self.lazy:
+            self.evaluate()
 
     def evaluate(self) -> None:
         if self.dirty:
+            old_value = self.value
             self.value = self.get()
             self.dirty = False
+            if self.callback:
+                self.callback(old_value, self.value)
 
     def get(self) -> Any:
         Dep.stack.append(self)
         try:
             value = self.fn()
         finally:
+            if self.deep:
+                traverse(value)
             Dep.stack.pop()
             self.cleanup_deps()
         return value
@@ -84,6 +105,8 @@ class Watcher:
         self._new_deps.clear()
 
     def depend(self) -> None:
+        """This function is used by other watchers to depend on everything
+        this watcher depends on."""
         if Dep.stack:
             for dep in self._deps:
                 dep.depend()
@@ -97,29 +120,60 @@ class Watcher:
 
 
 class ObservableDict(dict):
-    # TODO __len__, __delitem__, __iter__, __reversed__, etc?
+    # TODO cover all important methods
     def __getitem__(self, key: Any) -> Any:
         if Dep.stack:
             get_dep(self, key).depend()
         return super().__getitem__(key)
+
+    def values(self):
+        if Dep.stack:
+            get_dep(self).depend()
+        return super().values()
 
     def __setitem__(self, key: Any, new_value: Any) -> None:
         value = super().__getitem__(key)
         super().__setitem__(key, new_value)
         if new_value != value:
             get_dep(self, key).notify()
+            get_dep(self).notify()
 
 
 class ObservableList(list):
+    # TODO cover all important methods
     def __getitem__(self, key: Any) -> Any:
         if Dep.stack:
             get_dep(self).depend()
         return super().__getitem__(key)
 
+    def __iter__(self):
+        if Dep.stack:
+            get_dep(self).depend()
+        return super().__iter__()
+
     def __setitem__(self, key: Any, new_value: Any) -> None:
         value = super().__getitem__(key)
         super().__setitem__(key, new_value)
         if new_value != value:
+            get_dep(self).notify()
+
+
+class ObservableSet(set):
+    # TODO cover all important methods
+    def __contains__(self, obj: Any) -> Any:
+        if Dep.stack:
+            get_dep(self).depend()
+        return super().__contains__(obj)
+
+    def __iter__(self):
+        if Dep.stack:
+            get_dep(self).depend()
+        return super().__iter__()
+
+    def add(self, new_value: Any) -> None:
+        is_new = new_value not in self
+        super().add(new_value)
+        if is_new:
             get_dep(self).notify()
 
 
@@ -138,6 +192,17 @@ def make_reactive(obj, deep=True):
             for i, v in enumerate(reactive):
                 reactive[i] = make_reactive(v)
         return reactive
+    elif isinstance(obj, tuple):
+        reactive = obj  # tuples are immutable
+        if deep:
+            reactive = tuple(make_reactive(v) for v in reactive)
+        return reactive
+    elif isinstance(obj, set):
+        return (
+            ObservableSet(obj)
+            if not deep
+            else ObservableSet(make_reactive(v) for v in obj)
+        )
     else:
         raise NotImplementedError(f"Don't know how to make {type(obj)} reactive")
 
@@ -153,12 +218,14 @@ def cached(fn):
             watcher.depend()
         return watcher.value
 
-    getter.watcher = watcher
+    getter.__watcher__ = watcher
     return getter
 
 
 if __name__ == "__main__":
-    a = make_reactive({"foo": 5, "bar": [6, 7, 8], "quux": 10, "quuz": {"a": 1, "b": 2}})
+    a = make_reactive(
+        {"foo": 5, "bar": [6, 7, 8], "quux": 10, "quuz": {"a": 1, "b": 2}}
+    )
     execute_count = 0
 
     def bla():
@@ -184,4 +251,33 @@ if __name__ == "__main__":
     assert cached_bla() == 120
     assert execute_count == 3
 
-    del cached_bla
+    def bla2():
+        global execute_count
+        execute_count += 1
+        return a["foo"] * cached_bla()
+
+    cached_bla2 = cached(bla2)
+    assert cached_bla2() == 600
+    assert cached_bla2() == 600
+    assert execute_count == 4
+    a["quuz"]["b"] = 4
+    assert cached_bla2() == 800
+    assert cached_bla2() == 800
+    assert execute_count == 6
+
+    called = 0
+
+    def _callback(old_value, new_value):
+        global called
+        called += 1
+
+    watcher = Watcher(lambda: a["quuz"], lazy=False, deep=True, callback=_callback)
+    assert not watcher.dirty
+    assert watcher.value == a["quuz"]
+    assert len(watcher._deps) == 2
+    assert called == 0
+    a["quuz"]["b"] = 3
+    assert not watcher.dirty
+    assert watcher.value == a["quuz"]
+    assert len(watcher._deps) == 2
+    assert called == 1
