8df5bfbf194aad268b199f0f14148affe739303f
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2020-07-09 22:03:59 +0200

    prove our property

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -2,12 +2,24 @@ from observ import computed, observe
 
 
 def test_deps_copy():
+    # this test proves that even tho we have redundant deps
+    # we don't redundantly recompute
     state = observe({"foo": 5, "bar": 6})
+    call_count = 0
 
     @computed
     def prop():
+        nonlocal call_count
+        call_count += 1
         return state.copy()
 
     assert prop() == {"foo": 5, "bar": 6}
-    # reproduce issue #16
+    assert prop() is not state
     assert len(prop.__watcher__._deps) == 3
+    assert call_count == 1
+
+    state["foo"] = 3
+    assert prop() == {"foo": 3, "bar": 6}
+    assert prop() is not state
+    assert len(prop.__watcher__._deps) == 3
+    assert call_count == 2
