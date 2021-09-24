f378393ef39ce8dac79e92a03a491fa728344247
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2021-09-24 11:36:48 +0200

    make conftest a little smarter in resetting the old request flush

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,12 @@ def noop():
 
 @pytest.fixture
 def noop_request_flush():
+    old_callback = scheduler.request_flush
     scheduler.register_request_flush(noop)
     try:
         yield
     finally:
-        scheduler.register_request_flush(scheduler.request_flush_raise)
+        scheduler.register_request_flush(old_callback)
 
 
 @pytest.fixture(autouse=True)
