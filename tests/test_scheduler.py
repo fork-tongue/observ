74b1d6223d25441e2351c0173e48cca5b8f86b19
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2021-09-24 13:44:00 +0200

    found infinite loop that goes undetected

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -118,3 +118,42 @@ def test_queue_growth(noop_request_flush):
     assert len(scheduler._queue_indices) == 0
     assert calls_1 == 1
     assert calls_2 == 1
+
+
+def test_queue_growth_branching(noop_request_flush):
+    state = observe({"foo": 5, "bar": 6})
+    calls_1 = 0
+    calls_2 = 0
+
+    def exp_1():
+        if state["foo"] == 5:
+            return state["foo"]
+        else:
+            return state["bar"]
+
+    def cb_1(old, new):
+        nonlocal calls_1
+        calls_1 += 1
+        state["bar"] += 1
+
+    def cb_2(old, new):
+        nonlocal calls_2
+        state["foo"] += 1
+        calls_2 += 1
+
+    watcher_1 = watch(lambda: state["foo"], cb_1)  # noqa: F841
+    watcher_2 = watch(lambda: state["bar"], cb_2)  # noqa: F841
+
+    state["bar"] += 1
+
+    assert len(scheduler._queue) == 1
+    assert len(scheduler._queue_indices) == 1
+    assert calls_1 == 0
+    assert calls_2 == 0
+
+    scheduler.flush()
+
+    assert len(scheduler._queue) == 0
+    assert len(scheduler._queue_indices) == 0
+    assert calls_1 == 1
+    assert calls_2 == 1
