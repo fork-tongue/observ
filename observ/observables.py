3109ca1d74b6b0e682cc162565aa7aa541410cc6
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2021-09-27 18:07:21 +0200

    first pass

--- observ/observables.py
+++ observ/observables.py
@@ -8,138 +8,171 @@ import sys
 from .dep import Dep
 
 
-def observe(obj, deep=True):
+def observe(obj, deep=True, readonly=False):
     """Please be aware: this only works on plain data types!"""
     if not isinstance(obj, (dict, list, tuple, set)):
         return obj  # common case first
     elif isinstance(obj, dict):
-        if not isinstance(obj, ObservableDict):
-            reactive = ObservableDict(obj)
+        if not isinstance(obj, DictProxyBase):
+            reactive = ReadonlyDictProxy(obj) if readonly else DictProxy(obj)
         else:
             reactive = obj
         if deep:
             for k, v in reactive.items():
-                reactive[k] = observe(v)
+                reactive[k] = observe(v, deep=deep, readonly=readonly)
         return reactive
     elif isinstance(obj, list):
-        if not isinstance(obj, ObservableList):
-            reactive = ObservableList(obj)
+        if not isinstance(obj, ListProxyBase):
+            reactive = ReadonlyListProxy(obj) if readonly else ListProxy(obj)
         else:
             reactive = obj
         if deep:
             for i, v in enumerate(reactive):
-                reactive[i] = observe(v)
+                reactive[i] = observe(v, deep=deep, readonly=readonly)
         return reactive
     elif isinstance(obj, tuple):
         reactive = obj  # tuples are immutable
         if deep:
-            reactive = tuple(observe(v) for v in reactive)
+            reactive = tuple(observe(v, deep=deep, readonly=readonly) for v in reactive)
         return reactive
     elif isinstance(obj, set):
+        cls = ReadonlySetProxy if readonly else SetProxy
         if deep:
-            return ObservableSet(observe(v) for v in obj)
+            return cls({observe(v, deep=deep, readonly=readonly) for v in obj})
         else:
-            if not isinstance(obj, ObservableSet):
-                reactive = ObservableSet(obj)
+            if not isinstance(obj, SetProxyBase):
+                reactive = cls(obj)
             else:
                 reactive = obj
             return reactive
 
 
-def make_observable(cls):
-    def read(fn):
-        @wraps(fn)
-        def inner(self, *args, **kwargs):
-            if Dep.stack:
-                self.__dep__.depend()
-            return fn(self, *args, **kwargs)
+def read_trap(method, proxy_cls, obj_cls):
+    fn = getattr(obj_cls, method)
+    @wraps(fn)
+    def inner(self, *args, **kwargs):
+        if Dep.stack:
+            self.__dep__.depend()
+        return fn(self, *args, **kwargs)
 
-        return inner
+    return inner
 
-    def read_key(fn):
-        @wraps(fn)
-        def inner(self, *args, **kwargs):
-            if Dep.stack:
-                key = args[0]
-                self.__keydeps__[key].depend()
-            return fn(self, *args, **kwargs)
 
-        return inner
-
-    def write(fn):
-        @wraps(fn)
-        def inner(self, *args, **kwargs):
-            args = tuple(observe(a) for a in args)
-            kwargs = {k: observe(v) for k, v in kwargs.items()}
-            retval = fn(self, *args, **kwargs)
-            # TODO prevent firing if value hasn't actually changed?
-            self.__dep__.notify()
-            return retval
-
-        return inner
-
-    def write_key(fn):
-        @wraps(fn)
-        def inner(self, *args, **kwargs):
+def read_key_trap(method, proxy_cls, obj_cls):
+    fn = getattr(obj_cls, method)
+    @wraps(fn)
+    def inner(self, *args, **kwargs):
+        if Dep.stack:
             key = args[0]
-            is_new = key not in self.__keydeps__
-            old_value = cls.__getitem__(self, key) if not is_new else None
-            args = [key] + [observe(a) for a in args[1:]]
-            kwargs = {k: observe(v) for k, v in kwargs.items()}
-            retval = fn(self, *args, **kwargs)
-            new_value = cls.__getitem__(self, key)
-            if is_new:
-                self.__keydeps__[key] = Dep()
-            if old_value != new_value:
-                self.__keydeps__[key].notify()
-                self.__dep__.notify()
-            return retval
+            self.__keydeps__[key].depend()
+        return fn(self, *args, **kwargs)
 
-        return inner
+    return inner
 
-    def delete(fn):
-        @wraps(fn)
-        def inner(self, *args, **kwargs):
-            retval = fn(self, *args, **kwargs)
+
+def write_trap(method, proxy_cls, obj_cls):
+    fn = getattr(obj_cls, method)
+    @wraps(fn)
+    def inner(self, *args, **kwargs):
+        args = tuple(observe(a) for a in args)
+        kwargs = {k: observe(v) for k, v in kwargs.items()}
+        retval = fn(self, *args, **kwargs)
+        # TODO prevent firing if value hasn't actually changed?
+        self.__dep__.notify()
+        return retval
+
+    return inner
+
+
+def write_key_trap(method, proxy_cls, obj_cls):
+    fn = getattr(obj_cls, method)
+    getitem_fn = getattr(obj_cls, "__getitem__")
+    @wraps(fn)
+    def inner(self, *args, **kwargs):
+        key = args[0]
+        is_new = key not in self.__keydeps__
+        old_value = getitem_fn(self, key) if not is_new else None
+        args = [key] + [observe(a) for a in args[1:]]
+        kwargs = {k: observe(v) for k, v in kwargs.items()}
+        retval = fn(self, *args, **kwargs)
+        new_value = getitem_fn(self, key)
+        if is_new:
+            self.__keydeps__[key] = Dep()
+        if old_value != new_value:
+            self.__keydeps__[key].notify()
             self.__dep__.notify()
-            for key in self._orphaned_keydeps():
-                self.__keydeps__[key].notify()
-                del self.__keydeps__[key]
-            return retval
+        return retval
 
-        return inner
+    return inner
 
-    def delete_key(fn):
-        @wraps(fn)
-        def inner(self, *args, **kwargs):
-            retval = fn(self, *args, **kwargs)
-            key = args[0]
-            self.__dep__.notify()
+
+def delete_trap(method, proxy_cls, obj_cls):
+    fn = getattr(obj_cls, method)
+    @wraps(fn)
+    def inner(self, *args, **kwargs):
+        retval = fn(self, *args, **kwargs)
+        self.__dep__.notify()
+        for key in self._orphaned_keydeps():
             self.__keydeps__[key].notify()
             del self.__keydeps__[key]
-            return retval
+        return retval
 
-        return inner
-
-    todo = [
-        ("_READERS", read),
-        ("_KEYREADERS", read_key),
-        ("_WRITERS", write),
-        ("_KEYWRITERS", write_key),
-        ("_DELETERS", delete),
-        ("_KEYDELETERS", delete_key),
-    ]
-
-    for category, decorate in todo:
-        for name in getattr(cls, category, set()):
-            fn = getattr(cls, name)
-            setattr(cls, name, decorate(fn))
-
-    return cls
+    return inner
 
 
-class ObservableDict(dict):
-    _READERS = {
+def delete_key_trap(method, proxy_cls, obj_cls):
+    fn = getattr(obj_cls, method)
+    @wraps(fn)
+    def inner(self, *args, **kwargs):
+        retval = fn(self, *args, **kwargs)
+        key = args[0]
+        self.__dep__.notify()
+        self.__keydeps__[key].notify()
+        del self.__keydeps__[key]
+        return retval
+
+    return inner
+
+
+trap_map = {
+    "READERS": read_trap,
+    "KEYREADERS": read_key_trap,
+    "WRITERS": write_trap,
+    "KEYWRITERS": write_key_trap,
+    "DELETERS": delete_trap,
+    "KEYDELETERS": delete_key_trap,
+}
+
+class ReadonlyError(Exception):
+    pass
+
+
+def readonly_trap(method, proxy_cls, obj_cls):
+    fn = getattr(obj_cls, method)
+    @wraps(fn)
+    def inner(self, *args, **kwargs):
+        raise ReadonlyError()
+
+    return inner
+
+trap_map_readonly = {
+    "READERS": read_trap,
+    "KEYREADERS": read_key_trap,
+    "WRITERS": readonly_trap,
+    "KEYWRITERS": readonly_trap,
+    "DELETERS": readonly_trap,
+    "KEYDELETERS": readonly_trap,
+}
+
+def make_observable(proxy_cls, obj_cls, traps, trap_map):
+    for trap_type, methods in traps.items():
+        for method in methods:
+            trap = trap_map[trap_type](method, proxy_cls, obj_cls)
+            setattr(proxy_cls, method, trap)
+
+
+dict_traps = {
+    "READERS": {
         "values",
         "copy",
         "items",
@@ -157,49 +190,65 @@ class ObservableDict(dict):
         "__sizeof__",
         "__str__",
     }
-    _KEYREADERS = {
+    "KEYREADERS": {
         "get",
         "__contains__",
         "__getitem__",
     }
-    _WRITERS = {
+    "WRITERS": {
         "update",
     }
-    _KEYWRITERS = {
+    "KEYWRITERS": {
         "setdefault",
         "__setitem__",
     }
-    _DELETERS = {
+    "DELETERS": {
         "clear",
         "popitem",
     }
-    _KEYDELETERS = {
+    "KEYDELETERS": {
         "pop",
         "__delitem__",
-    }
-
-    @wraps(dict.__init__)
-    def __init__(self, *args, **kwargs):
-        dict.__init__(self, *args, **kwargs)
-        self.__dep__ = Dep()
-        self.__keydeps__ = {key: Dep() for key in dict.keys(self)}
-
-    def _orphaned_keydeps(self):
-        return set(self.__keydeps__.keys()) - set(dict.keys(self))
-
+    },
+}
 
 if sys.version_info >= (3, 8, 0):
-    ObservableDict._READERS.add("__reversed__")
+    dict_traps["READERS"].add("__reversed__")
 if sys.version_info >= (3, 9, 0):
-    ObservableDict._READERS.add("__or__")
-    ObservableDict._READERS.add("__ror__")
-    ObservableDict._WRITERS.add("__ior__")
-ObservableDict = make_observable(ObservableDict)
+    dict_traps["READERS"].add("__or__")
+    dict_traps["READERS"].add("__ror__")
+    dict_traps["WRITERS"].add("__ior__")
 
 
-@make_observable
-class ObservableList(list):
-    _READERS = {
+class ProxyBase:
+    def __init__(self, obj):
+        self._obj = obj
+
+
+class DictProxyBase(ProxyBase):
+    def __init__(self, obj):
+        super().__init__(obj)
+        self.__dep__ = Dep()
+        self.__keydeps__ = {key: Dep() for key in self._obj.keys()}
+
+    def _orphaned_keydeps(self):
+        return set(self.__keydeps__.keys()) - set(self._obj.keys())
+
+
+class DictProxy(DictProxyBase):
+    pass
+
+
+class ReadonlyDictProxy(DictProxyBase):
+    pass
+
+
+make_observable(DictProxy, dict, dict_traps, trap_map)
+make_observable(ReadonlyDictProxy, dict, dict_traps, trap_map_readonly)
+
+
+list_traps = {
+    "READERS": {
         "count",
         "index",
         "copy",
@@ -222,7 +271,7 @@ class ObservableList(list):
         "__reversed__",
         "__sizeof__",
     }
-    _WRITERS = {
+    "WRITERS": {
         "append",
         "clear",
         "extend",
@@ -236,16 +285,29 @@ class ObservableList(list):
         "__iadd__",
         "__imul__",
     }
+}
 
-    @wraps(list.__init__)
-    def __init__(self, *args, **kwargs):
-        list.__init__(self, *args, **kwargs)
+
+class ListProxyBase(ProxyBase):
+    def __init__(self, obj):
+        super().__init__(obj)
         self.__dep__ = Dep()
 
 
-@make_observable
-class ObservableSet(set):
-    _READERS = {
+class ListProxy(ListProxyBase):
+    pass
+
+
+class ReadonlyListProxy(ListProxyBase):
+    pass
+
+
+make_observable(ListProxy, list, list_traps, trap_map)
+make_observable(ReadonlyListProxy, list, list_traps, trap_map_readonly)
+
+
+set_traps = {
+    "READERS": {
         "copy",
         "difference",
         "intersection",
@@ -280,7 +342,7 @@ class ObservableSet(set):
         "__sub__",
         "__xor__",
     }
-    _WRITERS = {
+    "WRITERS": {
         "add",
         "clear",
         "difference_update",
@@ -291,8 +353,22 @@ class ObservableSet(set):
         "symmetric_difference_update",
         "update",
     }
+}
 
-    @wraps(set.__init__)
-    def __init__(self, *args, **kwargs):
-        set.__init__(self, *args, **kwargs)
+
+class SetProxyBase(ProxyBase):
+    def __init__(self, obj):
+        super().__init__(obj)
         self.__dep__ = Dep()
+
+
+class SetProxy(SetProxyBase):
+    pass
+
+
+class ReadonlySetProxy(SetProxyBase):
+    pass
+
+
+make_observable(SetProxy, set, set_traps, trap_map)
+make_observable(ReadonlySetProxy, set, set_traps, trap_map_readonly)
