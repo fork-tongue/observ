ef763817e3baf7965dea0a466b11d305c70574e2
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2023-11-02 15:44:02 +0100

    Add test for watch effect with scheduler

--- tests/test_watch_effect.py
+++ tests/test_watch_effect.py
@@ -1,4 +1,4 @@
-from observ import reactive
+from observ import reactive, scheduler
 from observ.watcher import watch_effect
 
 
@@ -17,6 +17,27 @@ def test_watch_effect():
     assert count_mirror == 0
 
     state["count"] = 1
+
+    assert state["count"] == 1
+    assert count_mirror == 1
+
+
+def test_watch_effect_scheduled(noop_request_flush):
+    state = reactive({"count": 0})
+    count_mirror = -1
+
+    def bump():
+        nonlocal count_mirror
+        count_mirror = state["count"]
+
+    watcher = watch_effect(bump)
+    assert watcher.lazy is False
+
+    assert state["count"] == 0
+    assert count_mirror == 0
+
+    state["count"] = 1
+    scheduler.flush()
 
     assert state["count"] == 1
     assert count_mirror == 1
