9505401bf19c88d9ffc7eb1f30aacf132b6e65eb
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2023-11-07 15:45:47 +0100

    add benchmarking setup

--- bench/test_bench.py
+++ bench/test_bench.py
@@ -5,16 +5,62 @@ import pytest
 from observ import reactive
 
 
-def bench_dict(obj):
-    for i in range(10000):
-        obj[str(i)] = "foo"
+def bench_dict(plain, n=1000):
+    for _ in range(n):
+        obj = {} if plain else reactive({})
+        obj["bar"] = "baz"
+        obj["quux"] = "quuz"
+        obj.update({
+            "bar": "foo",
+            "quazi": "var",
+        })
+        del obj["bar"]
+        assert obj["quux"]  # read something
+        obj.clear()
 
 
+@pytest.mark.benchmark(
+    group="dict_plain_vs_reactive",
+)
 @pytest.mark.parametrize("name", ["plain", "reactive"])
-def test_bench_dict(benchmark, name):
-    obj = {}
-    if name == "reactive":
-        obj = reactive(obj)
-    bench_fn = partial(bench_dict, obj)
+def test_dict_plain_vs_reactive(benchmark, name):
+    bench_fn = partial(bench_dict, name == "plain")
+    benchmark(bench_fn)
 
+
+def bench_list(plain, n=1000):
+    for _ in range(n):
+        obj = [] if plain else reactive([])
+        obj.append("bar")
+        obj.extend(["quux", "quuz"])
+        obj[1] = "foo"
+        obj.pop(0)
+        assert obj[0]  # read something
+        obj.clear()
+
+
+@pytest.mark.benchmark(
+    group="list_plain_vs_reactive",
+)
+@pytest.mark.parametrize("name", ["plain", "reactive"])
+def test_list_plain_vs_reactive(benchmark, name):
+    bench_fn = partial(bench_list, name == "plain")
+    benchmark(bench_fn)
+
+
+def bench_set(plain, n=1000):
+    for _ in range(n):
+        obj = set() if plain else reactive(set())
+        obj.add("bar")
+        obj.update({"quux", "quuz"})
+        assert list(iter(obj))  # read something
+        obj.clear()
+
+
+@pytest.mark.benchmark(
+    group="set_plain_vs_reactive",
+)
+@pytest.mark.parametrize("name", ["plain", "reactive"])
+def test_set_plain_vs_reactive(benchmark, name):
+    bench_fn = partial(bench_set, name == "plain")
     benchmark(bench_fn)
