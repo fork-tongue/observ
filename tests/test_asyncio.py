addec0c83ebc64886464faf959c65ea75e38dc7a
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2023-11-14 16:27:02 +0100

    add negative test

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -7,13 +7,13 @@ from observ import reactive, watch, watch_effect
 
 
 @pytest.fixture
-def ensure_loop():
+def plain_loop():
     loop = asyncio.new_event_loop()
     asyncio.get_event_loop_policy().set_event_loop(loop)
     return loop
 
 
-def test_asyncio_watch_callback(ensure_loop):
+def test_asyncio_watch_callback(plain_loop):
     a = reactive([1, 2])
     called = 0
 
@@ -31,7 +31,7 @@ def test_asyncio_watch_callback(ensure_loop):
     assert len(a) == 3
 
 
-def test_asyncio_watch_expression_not_supported(ensure_loop):
+def test_asyncio_watch_expression_not_supported(plain_loop):
     a = reactive([1, 2])
 
     async def _expr():
@@ -47,22 +47,21 @@ def test_asyncio_watch_expression_not_supported(ensure_loop):
 
 
 @pytest.fixture
-def eager_loop(ensure_loop):
+def eager_loop(plain_loop):
     # only python>=3.12
     if not getattr(asyncio, "eager_task_factory", None):
         yield
         return
 
-    loop = ensure_loop
-    old_factory = loop.get_task_factory()
-    loop.set_task_factory(asyncio.eager_task_factory)
+    old_factory = plain_loop.get_task_factory()
+    plain_loop.set_task_factory(asyncio.eager_task_factory)
     try:
-        yield loop
+        yield plain_loop
     finally:
-        loop.set_task_factory(old_factory)
+        plain_loop.set_task_factory(old_factory)
 
 
-def test_asyncio_watch_effect(eager_loop):
+def test_asyncio_watch_effect_eager_loop(eager_loop):
     a = reactive([1, 2])
     called = 0
 
@@ -90,3 +89,27 @@ def test_asyncio_watch_effect(eager_loop):
 
     assert called == 2
     assert len(a) == 3
+
+
+def test_asyncio_watch_effect_plain_loop(plain_loop):
+    a = reactive([1, 2])
+    called = 0
+
+    async def _expr():
+        nonlocal called
+        len_a = len(a)  # noqa: F841
+        await asyncio.sleep(0)
+        called += 1
+
+    if sys.version_info < (3, 12, 0):
+        with pytest.raises(RuntimeError):
+            watcher = watch_effect(_expr, sync=True)
+        return
+
+    async def _coroutine():
+        return watch_effect(_expr, sync=True)
+
+    watcher = plain_loop.run_until_complete(_coroutine())  # noqa: F841
+
+    with pytest.xfail("doesn't work without eager loop"):
+        assert called == 1
